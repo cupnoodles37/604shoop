4d5bcfa9360d0749d1c123f57e51e35f4aaefcd6
cupnoodles37 <roopre48@daum.net>
2024-07-09 12:19:39 +0900

    Update ProductDetail.js

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -19,7 +19,7 @@ function ProductDetail() {
   const [product, setProcudt] = useState(null);
   // console.log(id)
   const getProductDetail = async () => {
-    let url = `http://localhost:3005/products/${id}`;
+    let url = `https://my-json-server.typicode.com/cupnoodles37/604shoop/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
